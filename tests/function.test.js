9b7064731fd0cce2953f74f51e11af2d0612da29
inakiesk <inakieskruiz@gmail.com>
2022-09-02 13:16:31 -0500

    Fix code comments

--- tests/function.test.js
+++ tests/function.test.js
@@ -86,12 +86,13 @@ describe('Testing', () => {
     const oldLocal = JSON.parse(localStorage.getItem('task'));
     clearList.clearAll();
     const newLocal = JSON.parse(localStorage.getItem('task'));
+    /* Clear all check test */
     expect(
       clearList.listArr,
     ).toHaveLength(1);
+    /* Local storage change check */
     expect(
       oldLocal,
     ).not.toEqual(newLocal);
   });
 });
-/* Comment */
